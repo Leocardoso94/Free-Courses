d4fee1236742bb74d56a86304629ad27e104e255
Leonardo Cardoso <leonardo.cardoso@one.intranet>
2017-10-10 14:42:36 -0300

    course details

--- src/containers/Content/course.js
+++ src/containers/Content/course.js
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 import RoundedButton from './../../components/RoundedButton';
 
 class Course extends Component {
+	getImage() {
+		const defaultImage = 'https://cdn.dribbble.com/users/4764/screenshots/2418753/books_1x.png';
+		return !this.props.course.image || this.props.course.image === '' ? defaultImage : this.props.course.image;
+	}
 	render() {
 		return (
-			<div>
+			<div className="course-details">
 				<h1 className="title">{this.props.course.title}</h1>
+				<img src={this.getImage()} />
 				<h3>Author: {this.props.course.author}</h3>
 				<h4>Language: {this.props.course.language}</h4>
 				<h4>Level: {this.props.course.level}</h4>
