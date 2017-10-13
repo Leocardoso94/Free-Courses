6cbe2688b2d5a50cf1ad34f8c7b2ff80da32486d
Leonardo Cardoso <leonardo.cardoso@one.intranet>
2017-10-13 09:59:47 -0300

    teste

--- deploy.sh
+++ deploy.sh
@@ -17,14 +17,8 @@ git config user.email "leocardosoti@gmail.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 
-
-git remote add origin https://github.com/FreeCourses/freecourses.github.io.git
-
-git push --force  origin master
-
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-# git push --force  "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-
+ git push --force  "https://leocardoso94:${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
