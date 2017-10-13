5bd1e43265cd46e21fa46ffd2c6216c556c233f8
Leonardo Cardoso <leonardo.cardoso@one.intranet>
2017-10-13 09:54:48 -0300

    teste

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd public
 git init
 
 # inside this git repo we'll pretend to be a new user
-git config user.name "leocardoso94"
+git config user.name "Travis CI"
 git config user.email "leocardosoti@gmail.com"
 
 # The first and only commit to this new Git repo contains all the
@@ -17,9 +17,14 @@ git config user.email "leocardosoti@gmail.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
 
+
+git remote add origin https://github.com/FreeCourses/freecourses.github.io.git
+
+git push --force  origin master
+
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force  "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+# git push --force  "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
