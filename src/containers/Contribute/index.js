26974aa19a6ef3ee44b16d443357355fac6bddeb
Leonardo Cardoso <leocardosoti@gmail.com>
2017-10-17 11:11:01 -0200

    avatar on contribute page

--- src/containers/Contribute/index.js
+++ src/containers/Contribute/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './index.scss';
 import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
-
+import Contributor from './contributor';
 class Contribute extends Component {
 	constructor(props) {
 		super(props);
@@ -10,12 +10,19 @@ class Contribute extends Component {
 		fetch('https://api.github.com/repos/Leocardoso94/Free-Courses/contributors')
 			.then(blob => blob.json()
 				.then(data => this.updateContributors(data)));
+
+		this.contributorsList = this.contributorsList.bind(this);
 	}
 
 	updateContributors(contributors) {
 		this.setState({ contributors });
 	}
 
+	contributorsList() {
+		return this.state.contributors.map(contributor => {
+			return <Contributor contributor={contributor} key={contributor.login}/>;
+		});
+	}
 
 	render() {
 		return (
@@ -27,27 +34,31 @@ class Contribute extends Component {
 				transitionLeave={false}
 				component="div"
 				className="contribute">
-					<h1 className="title">Contribute</h1>
-					<p>If you know a free course that is not listed, please contribute. See how:</p>
+				<h1 className="title">Contribute</h1>
+				<p>If you know a free course that is not listed, please contribute. See how:
+				</p>
+
+				<ol>
+					<li><a href="https://github.com/Leocardoso94/Free-Courses#fork-destination-box" rel="noopener noreferrer" target="_blank">Fork this project</a></li>
+					<li>Go to the folder /src/data</li>
+					<li>To add a new category</li>
 					<ol>
-						<li><a href="https://github.com/Leocardoso94/Free-Courses#fork-destination-box" rel="noopener noreferrer" target="_blank">Fork this project</a></li>
-						<li>Go to the folder /src/data</li>
-						<li>To add a new category</li>
-						<ol>
-							<li>Open the categories.json</li>
-							<li>Add a new object, <a href="https://github.com/Leocardoso94/Free-Courses/blob/master/src/data/categories.json" rel="noopener noreferrer" target="_blank">example</a></li>
-						</ol>
-						<li>To add a new course</li>
-						<ol>
-							<li>Open the courses.json</li>
-							<li>Add a new object, <a href="https://github.com/Leocardoso94/Free-Courses/blob/master/src/data/courses.json" rel="noopener noreferrer" target="_blank">example</a></li>
-						</ol>
-						<li>After that do a Pull Request by telling what was added</li>
+						<li>Open the categories.json</li>
+						<li>Add a new object, <a href="https://github.com/Leocardoso94/Free-Courses/blob/master/src/data/categories.json" rel="noopener noreferrer" target="_blank">example</a></li>
 					</ol>
-					<h4>Contributors:</h4>
-					{this.state.contributors.map(contributor => {
-						return <a target="_blank" rel="noopener noreferrer" href={contributor.html_url} style={{ marginRight: "5px", display: 'inline-block', padding: '5px' }} key={contributor.login}>@{contributor.login}</a>;
-					})}
+					<li>To add a new course</li>
+					<ol>
+						<li>Open the courses.json</li>
+						<li>Add a new object, <a href="https://github.com/Leocardoso94/Free-Courses/blob/master/src/data/courses.json" rel="noopener noreferrer" target="_blank">example</a></li>
+					</ol>
+					<li>After that do a Pull Request by telling what was added</li>
+				</ol>
+
+				<h4>Contributors</h4>
+
+				<ul className="contributors">
+					{this.contributorsList()}
+				</ul>
 
 			</ReactCSSTransitionGroup>
 		);
