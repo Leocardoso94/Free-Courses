0e0747d21f63cc2c2ab710f71a042bb045fa414e
Leonardo Cardoso <leocardosoti@gmail.com>
2017-10-19 13:09:15 -0200

    ReducerCourses tests

--- src/reducers/reducer_categories.spec.js
+++ src/reducers/reducer_categories.spec.js
@@ -8,10 +8,10 @@ describe('ReducerCategories', function () {
 
 	describe('properties of elements', () => {
 		it('should return true if all object has a title', () => {
-			expect(ReducerCategories().every(cat => cat.title)).to.be.true;
+			expect(ReducerCategories().every(category => category.title)).to.be.true;
 		});
 		it('should return true if all object has a icon', () => {
-			expect(ReducerCategories().every(cat => cat.icon)).to.be.true;
+			expect(ReducerCategories().every(category => category.icon)).to.be.true;
 		});
 	});
 });
