b5099ed7d61306b9ea54e78cdfaee517bce94085
Leonardo Cardoso <leocardosoti@gmail.com>
2017-10-20 01:16:42 -0200

    share on twitter button

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 import Contributor from './contributor';
 import FaIcon from './../../components/Icons/fa-icon';
 import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
-
+import TweetButton from './../../components/TweetButton';
 
 class Home extends Component {
   constructor(props) {
@@ -60,6 +60,9 @@ class Home extends Component {
           <br />
           <br />
           <span >Made with <FaIcon icon="fa-heart" color="red" /> by <a target="_blank" rel="noopener noreferrer" href="https://github.com/Leocardoso94">@LeoCardoso94</a></span>
+          <br />
+          <br />
+          <TweetButton text="FreeCourses. A collection of free courses for developers https://freecourses.github.io/" title="Share" />
         </p>
         <h3 style={{ textAlign: 'left', marginTop: '40px' }}>Useful links:</h3>
         <ol style={{ textAlign: 'left' }}>
