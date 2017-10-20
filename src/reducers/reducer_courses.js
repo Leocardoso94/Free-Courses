a8a77226fa0f7f1a21be3c220f3aa400e848c770
Leonardo Cardoso <leocardosoti@gmail.com>
2017-10-20 08:36:19 -0200

    New format of courses

--- src/reducers/reducer_courses.js
+++ src/reducers/reducer_courses.js
@@ -2,7 +2,9 @@ import courses from './../data/courses.json';
 courses.forEach((course, index) => {
   course.id = index;
   if (typeof course.categories === 'string')
-    course.categories = course.categories.split(' ');
+    course.categories = course.categories.split(',');
+  if (typeof course.flags === 'string')
+    course.flags = course.flags.split(',');
 });
 
 export default function () {
