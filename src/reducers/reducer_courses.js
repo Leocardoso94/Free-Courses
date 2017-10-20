aba5cfdb6fc56315ed6cba1c033a58f8b9e678ed
Leonardo Cardoso <leocardosoti@gmail.com>
2017-10-20 08:09:02 -0200

    adding java course

--- src/reducers/reducer_courses.js
+++ src/reducers/reducer_courses.js
@@ -1,8 +1,10 @@
 import courses from './../data/courses.json';
 courses.forEach((course, index) => {
-	course.id = index;
+  course.id = index;
+  if (typeof course.categories === 'string')
+    course.categories = course.categories.split(' ');
 });
 
 export default function () {
-	return courses;
+  return courses;
 }
