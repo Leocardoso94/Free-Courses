9197bb0ad6d31a6e2d1b10f2aa1c2f69d9009ec1
Leonardo Cardoso <leocardosoti@gmail.com>
2017-10-26 11:42:02 -0200

    fixes

--- src/containers/Contribute/index.js
+++ src/containers/Contribute/index.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import './index.scss';
 import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 class Contribute extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { contributors: [] };
-
-
-  }
-
-
 
   render() {
     return (
@@ -25,7 +17,7 @@ class Contribute extends Component {
         <p>If you know a free course that is not listed, please contribute.
 				</p>
         <div className="iframe">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfhXYqkwSHAnVKcD4qvqCmtsZV4BUhr7XVzNB3fTko34-aIhw/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Carregando…</iframe>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfhXYqkwSHAnVKcD4qvqCmtsZV4BUhr7XVzNB3fTko34-aIhw/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Carregando…</iframe>
         </div>
       </ReactCSSTransitionGroup>
     );
