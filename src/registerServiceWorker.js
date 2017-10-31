deca58e73e366df672bc78a0f16bff2a385cc8b5
Leonardo Cardoso <leocardosoti@gmail.com>
2017-10-31 16:30:28 -0200

    update service Worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -24,25 +24,7 @@ function registerValidSW(swUrl) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
-            if (navigator.serviceWorker.controller) {
-              // At this point, the old content will have been purged and
-              // the fresh content will have been added to the cache.
-              // It's the perfect time to display a "New content is
-              // available; please refresh." message in your web app.
-              console.log('New content is available; please refresh.');
-            } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
-            }
-          }
-        };
-      };
+      registration.update();
     })
     .catch(error => {
       console.error('Error during service worker registration:', error);
