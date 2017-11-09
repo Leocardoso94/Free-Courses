aca224adbbe0993d2e1340196e5825fb83a17aab
Leonardo Cardoso <leocardosoti@gmail.com>
2017-11-09 10:05:23 -0200

    removing spaces before and after commas

--- src/reducers/reducer_courses.js
+++ src/reducers/reducer_courses.js
@@ -3,7 +3,7 @@ import snakeCase from 'lodash.snakecase';
 courses.forEach((course) => {
   course.id = snakeCase(course.title + course.author);
   if (typeof course.categories === 'string')
-    course.categories = course.categories.split(',');
+    course.categories = course.categories.replace(/ *, */g, ',').split(',');
   if (typeof course.flags === 'string')
     course.flags = course.flags.split(',');
 });
