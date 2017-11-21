56c763e60fcce601b4391fe1cebb40912d979e93
Leonardo Cardoso <leocardosoti@gmail.com>
2017-11-21 16:13:50 -0200

    removing images

--- src/containers/Category/course-list.js
+++ src/containers/Category/course-list.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 import Masonry from 'react-masonry-component';
 
 const CourseList = ({ coursesInCategory }) => {
-  function getImage(course) {
-    const defaultImage = 'https://cdn.dribbble.com/users/4764/screenshots/2418753/books_1x.png';
-    return !course.image || course.image === '' ? defaultImage : course.image;
-  }
+  // function getImage(course) {
+  //   const defaultImage = 'https://cdn.dribbble.com/users/4764/screenshots/2418753/books_1x.png';
+  //   return !course.image || course.image === '' ? defaultImage : course.image;
+  // }
 
   function getDescription(course) {
     let description = course.description.replace(/^(.{50}[^\s]*).*/, "$1");
@@ -23,9 +23,9 @@ const CourseList = ({ coursesInCategory }) => {
     return (
       <li className="course" key={course.title + course.author}>
         <Link to={`/course/${course.id}`}>
-          <div className="image">
+          {/* <div className="image">
             <img src={getImage(course)} />
-          </div>
+          </div> */}
           <div className="description">
             <h6 className="author">{course.author}</h6>
             <h4 className="title">{course.title}</h4>
