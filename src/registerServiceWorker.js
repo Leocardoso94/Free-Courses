cda1f21662f7f6533f04ff1eff17798023953496
Leonardo Cardoso <leocardosoti@gmail.com>
2018-01-18 12:11:32 -0200

    fix

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -72,7 +72,7 @@ export default function register() {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = './sw.js';
+      const swUrl = '/sw.js';
 
       if (!isLocalhost) {
         // Is not local host. Just register service worker
