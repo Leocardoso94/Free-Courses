b7f12650e04c78d17abc250a0c0fc584ab7dcd3d
Leonardo Cardoso <leonardo.cardoso@one.intranet>
2018-02-01 16:28:26 -0200

    fix import first

--- src/containers/Category/index.js
+++ src/containers/Category/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import { connect } from 'react-redux';
 import DevIcon from './../../components/Icons/dev-icon';
 import './index.scss';
-import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import CourseList from './course-list';
 import BackButton from './../../components/BackButton';
 import { fetchCourses } from '../../actions/index';
