a1ecc8e7a95a48b7d0231a15be676264a3617dca
Leonardo Cardoso <leocardosoti@gmail.com>
2020-10-26 12:06:29 -0300

    replace img src on error

--- src/containers/Category/course-list.tsx
+++ src/containers/Category/course-list.tsx
@@ -2,27 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Course } from "../../data/courses";
 
-const CourseList: React.FC<{ coursesInCategory: Course[] }> = ({
-  coursesInCategory
-}) => {
-  function getImage(course: Course) {
-    const defaultImage =
-      "https://cdn.dribbble.com/users/4764/screenshots/2418753/books_1x.png";
-    return !course.image || course.image === "" ? defaultImage : course.image;
-  }
+const defaultImage =
+  "https://cdn.dribbble.com/users/4764/screenshots/2418753/books_1x.png";
 
+function getImage(course: Course) {
+  return !course.image || course.image === "" ? defaultImage : course.image;
+}
+
+const CourseList: React.FC<{ coursesInCategory: Course[] }> = ({
+  coursesInCategory,
+}) => {
   const renderTags = (course: Course) =>
-    course.categories.map(categoryOfCourse => (
+    course.categories.map((categoryOfCourse) => (
       <Link className="tag" to={categoryOfCourse} key={categoryOfCourse}>
         {categoryOfCourse}
       </Link>
     ));
 
-  const courses = coursesInCategory.map(course => (
+  const courses = coursesInCategory.map((course) => (
     <li className="course" key={course.title + course.author}>
       <Link to={`/course/${course.id}`}>
         <div className="image">
-          <img src={getImage(course)} alt={course.title} />
+          <img
+            src={getImage(course)}
+            alt={course.title}
+            onError={(e) => {
+              //@ts-ignore
+              e.target.src = defaultImage;
+            }}
+          />
         </div>
         <div className="description">
           <h6 className="author">{course.author}</h6>
@@ -32,7 +40,7 @@ const CourseList: React.FC<{ coursesInCategory: Course[] }> = ({
       </Link>
       <div className="flags">
         {course.flags
-          ? course.flags.map(flag => (
+          ? course.flags.map((flag) => (
               <span key={flag} className={`flag-icon ${flag}`} />
             ))
           : ""}
